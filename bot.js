26012581d3a7995016f2169eab559106b49167c2
elanaliev <elanaliev@users.noreply.github.com>
2020-12-02 20:30:35 +0300

    1

--- bot.js
+++ bot.js
@@ -10,13 +10,46 @@ const token = JRead.ReadJson().getToken().token;
 let bot = new telegraf(token);
 
 bot.use((msg, next) => {
-    console.log(`Message from: ${msg.from.username}\nId: ${msg.from.id}\nMessage: ${msg.message.text}`);
-    msg.reply(msg.message);
+    console.log(`Message from: ${msg.from.username} - Id: ${msg.from.id}`);
     next();
 })
 
+bot.command('ban', (msg) => {
+    replyAction(msg, (from) => {
+        msg.reply('Ban ' + from.username);
+        msg.kickChatMember(from.id);
+    })
+})
+
+bot.command('unban', (msg) => {
+    replyAction(msg, (from) => {
+        msg.reply('Unban ' + from.username);
+        msg.unbanChatMember(from.id);
+    })
+})
+
+function replyAction(msg, action){
+    getAccess(msg, () => {
+        let unbanfrom = msg.message.reply_to_message.from; 
+        if(unbanfrom.is_bot === false){
+            action(unbanfrom);
+        }   
+    });
+}
+
+
 
 bot.launch();
+
+function getAccess(msg, action){
+    msg.getChatMember(msg.from.id).then((result) => {
+        console.log(result.status);
+        if(result.status == 'creator' || result.status == 'administrator'){
+            action();
+        }
+        else console.log('no access');
+    })
+}
 
 function CreateInlineKeyboard(buttons){
     let keyboard = {inline_keyboard: buttons}
