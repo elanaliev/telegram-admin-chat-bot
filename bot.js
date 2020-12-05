3befc2dc6f0c0ed9912e0e2415dc1eeae882fae1
elanaliev <elanaliev@users.noreply.github.com>
2020-12-05 13:13:40 +0300

    2

--- bot.js
+++ bot.js
@@ -1,45 +1,48 @@
-/*  
-    !- Beta v1.0 -!
-    https://github.com/elanaliev/telegram-admin-chat-bot
-    https://github.com/elanaliev
-*/
 const telegraf = require('telegraf').default;
 const JRead = require('./JsonRead');
 const token = JRead.ReadJson().getToken().token;
 
 let bot = new telegraf(token);
 
+const commands = {
+    ban : (msg, id) => {
+        replyAction(msg, (from) => {
+            msg.kickChatMember((id === undefined) ? from.id : id);
+        })
+    },
+    unban : (msg, id) => {
+        replyAction(msg, (from) => {
+            msg.unbanChatMember((id === undefined) ? from.id : id);
+            msg.reply('Done!');
+        })
+    }
+}
+const keyboards = {
+    admin : (from) => {
+        return [[{text:'Ban', callback_data:`{"ban":${from.id}}`}, {text:'Unban', callback_data:`{"unban":${from.id}}`}]]       
+    }
+}
+
 bot.use((msg, next) => {
     console.log(`Message from: ${msg.from.username} - Id: ${msg.from.id}`);
     next();
 })
 
-bot.command('ban', (msg) => {
+bot.command('admin', (msg) => {
     replyAction(msg, (from) => {
-        msg.kickChatMember(from.id, 400000).then((result) => {
-            msg.reply('Ban ' + `@${from.username}` + ' : ' + result);
-        })
+        msg.reply(`Меню функций к пользователю ${(from.username === undefined) ? from.first_name : '@' + from.username}.\nЧто вы хотите сделать?`, CreateInlineKeyboard(keyboards.admin(from)).mark)
     })
-})
-
-bot.command('unban', (msg) => {
-    replyAction(msg, (from) => {
-        msg.unbanChatMember(from.id).then((result) => {
-            msg.reply('Unban ' + `@${from.username}` + ' : ' + result);
-        });
-    })
-})
-
-function replyAction(msg, action){
+}).on('callback_query', (msg) => {
     getAccess(msg, () => {
-        let from = msg.message.reply_to_message.from; 
-        if(from.is_bot === false){
-            action(from);
-        }   
-    });
-}
+        let a = JSON.parse(msg.callbackQuery.data);
+        let key = Object.keys(a)[0];
+        commands[key](msg, a[key]);
+        msg.deleteMessage();
+    })
+})
 
-
+bot.command('ban', (msg) => commands.ban(msg));
+bot.command('unban', (msg) => commands.unban(msg));
 
 bot.launch();
 
@@ -53,6 +56,15 @@ function getAccess(msg, action){
     })
 }
 
+function replyAction(msg, action){
+    getAccess(msg, () => {
+        let from = (msg.message === undefined) ? msg.from : msg.message.reply_to_message.from;
+        if(from.is_bot === false){
+            action(from);
+        }   
+    });
+}
+
 function CreateInlineKeyboard(buttons){
     let keyboard = {inline_keyboard: buttons}
     return {
@@ -60,11 +72,3 @@ function CreateInlineKeyboard(buttons){
         mark: {reply_markup: keyboard}
     }
 }
-
-function ifNotNull(value, action, action2 = () => { console.log('Value is null'); }){
-    if(value !== null && value !== undefined) {
-       return action();
-    } else {
-       return action2();
-    }
-};
